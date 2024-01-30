3c1c6fd154f871db05883a2e321870eaae928e88
dome0luis0valentin <valentindome777@gmail.com>
2024-01-30 11:31:17 -0300

    agrego calculo de ganancia para valores fijos

--- consumer_fijos.py
+++ consumer_fijos.py
@@ -20,14 +20,13 @@ json_schema = StructType([
     StructField('name', StringType(), True),
     StructField('date', StringType(), True),
     StructField('value', DoubleType(), True),
-    StructField('description', StringType(), True)
 ])
 
 # Read data from Kafka topic, starting from the latest offset
 batch_df = spark.read \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "empresas") \
+    .option("subscribe", "datos_fijos") \
     .load()
 
 # Parse JSON data
@@ -35,39 +34,32 @@ json_df = batch_df.selectExpr("cast(value as string) as value")
 batch_data = json_df.withColumn("value", from_json(json_df["value"], json_schema)).select("value.*")
 
 # Ordena el DataFrame por la columna "date" de forma descendente y selecciona los últimos 10 registros
-agg_data = batch_data.groupBy("description").agg(expr("max(date) as max_date"), expr("last(value, true) as last_value"), expr("last(name, true) as name"))
+agg_data = batch_data.groupBy("name").agg(expr("max(date) as max_date"), expr("last(value, true) as last_value"), expr("last(name, true) as key"))
 
 df_actual = agg_data.orderBy(expr("last_value").desc())
 
+df_actual.show()
+# Guardar el DataFrame en un archivo Parquet
+
+df_actual.write.mode("overwrite").parquet("./datos_fijos_iniciales")
 
 #Cargo los datos iniciales con los que voy a calcular la variación de los valores de las acciones
 
 # df_actual = agg_data.orderBy(expr("last_value").desc()).limit(10)
 
-# # Write the result to Parquet files
-# df_actual.write \
-#     .mode("overwrite") \
-#     .parquet("./datos_iniciales/")
-
 # df_actual.show()
-
-
 
     # Crear la sesión de Spark
 spark = SparkSession.builder.appName("Lectura de Parquet").getOrCreate()
 
 #     # Leer el archivo Parquet
-df_pivot = spark.read.parquet("./datos_iniciales/")
+df_pivot = spark.read.parquet("./datos_fijos_iniciales/")
 
-
-#     # Mostrar el resultado en pantalla
-
-# Rename columns
-df_pivot = df_pivot.withColumnRenamed("last_value", "valor_actual")
+# # Rename columns
+df_pivot = df_pivot.withColumnRenamed("last_value", "valor_inicial")
 df_pivot = df_pivot.withColumnRenamed("max_date", "initial_date")
-df_pivot = df_pivot.withColumnRenamed("description", "initial_description")
 
-df_actual = df_actual.withColumnRenamed("last_value", "valor_inicial")
+df_actual = df_actual.withColumnRenamed("last_value", "valor_actual")
 df_actual = df_actual.withColumnRenamed("max_date", "actual_date")
 df_actual = df_actual.withColumnRenamed("name", "actual_name")
 
@@ -79,7 +71,7 @@ difference_df = df_pivot.join(
     df_actual,
     df_pivot["name"] == df_actual["actual_name"],  # Specify a common column for joining
     "inner"
-).withColumn("difference", df_pivot["valor_actual"] - df_actual["valor_inicial"])
+).withColumn("difference", df_pivot["valor_inicial"] - df_actual["valor_actual"])
 
 # Calculate the percentage difference
 difference_df = difference_df.withColumn("difference_percentage", (difference_df["difference"] / difference_df["valor_inicial"]) * 100)
@@ -93,23 +85,19 @@ difference_df = difference_df.withColumn("formatted_date", date_format(differenc
 # difference_df.write \
 #     .partitionBy("formatted_d ate") \
 #     .mode("overwrite") \
-#     .parquet("./resultado/")
+#     .parquet("./resultados_fijos/")
 
-difference_df.select("initial_description", "initial_date", "formatted_date", "valor_inicial", "valor_actual", "difference", "difference_percentage").write \
-    .partitionBy("formatted_date") \
+difference_df.select("name", "initial_date", "formatted_date", "valor_inicial", "valor_actual", "difference", "difference_percentage").write \
+    .partitionBy("name", "formatted_date") \
     .mode("overwrite") \
-    .parquet("./resultado_empresas/")
+    .parquet("./resultado/")
 
-difference_df.select("initial_description", "initial_date", "formatted_date", "valor_inicial", "valor_actual", "difference", "difference_percentage").show()
+difference_df.select("name", "initial_date", "formatted_date", "valor_inicial", "valor_actual", "difference", "difference_percentage").show()
 
-total_percentage = difference_df.agg({"difference_percentage": "sum"}).collect()[0][0]
+# total_percentage = difference_df.agg({"difference_percentage": "sum"}).collect()[0][0]
+total_percentage_usd = difference_df.filter(difference_df.name == "usd").agg({"difference_percentage": "sum"}).collect()[0][0]
+total_plazo_fijo = difference_df.filter(difference_df.name == "plazo_fijo").agg({"difference_percentage": "sum"}).collect()[0][0]
 
-capital_inicial = 1000000
-capital_por_accion = capital_inicial / 10
-percentage_per_action = total_percentage / 10
-gana_por_accion = capital_por_accion * percentage_per_action / 100
-
-ganancia_total = gana_por_accion * 10
 print("\n\n\n\n\n\n\n\n\n")
-print("El total de la variación porcentual es: {:.2f}% por acción. (en promedio)".format(total_percentage/10))
-print(f"La ganancia es de: {ganancia_total:.2f}  pesos.")
+print(f"El dolar a variado en un: {total_percentage_usd:.2f}%")
+print(f"Tu inversión en plazo fijo a generado: {total_plazo_fijo:.2f}%")
---
+++ datos_fijos_producer.py
@@ -0,0 +1,63 @@
+from confluent_kafka import Producer
+import json
+import requests
+from api import obtener_precios_acciones
+from api import obtener_valor
+
+from pprint import pprint
+def fetch_api_data(api_url):
+    response = requests.get(api_url)
+    return response.json()
+
+def modificar_formato(lista, name):
+    nueva_lista = []
+    for diccionario in lista:
+        nuevo_diccionario = {
+            "name": name,
+            "date": diccionario["d"],
+            "value": diccionario["v"]
+        }
+        nueva_lista.append(nuevo_diccionario)
+    return nueva_lista
+
+def kafka_producer():
+    """
+    Function that produces data to a Kafka topic.
+
+    This function retrieves stock prices and API data, and sends them to a Kafka topic called 'api_data_topic'.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    
+    producer = Producer({'bootstrap.servers': 'localhost:9092'})
+
+    api_urls = ["https://api.estadisticasbcra.com/inflacion_mensual_oficial",
+                "https://api.estadisticasbcra.com/plazo_fijo",
+                "https://api.estadisticasbcra.com/usd"]
+
+    for api_url in api_urls:
+        api_data = obtener_valor(api_url)
+        api_data = api_data[len(api_data)-30:]
+        print(api_url)
+
+        titulo = api_url.split("/")[-1]
+        dict = modificar_formato(api_data, titulo)
+
+        for i, diccionario in enumerate(dict):
+        #Agregamos el mensaje recibido de la API al topic
+            print(diccionario)
+            producer.produce('datos_fijos', key=titulo, value=json.dumps(diccionario))
+
+    producer.flush() # Ensure all messages are sent before exiting
+
+#Cheque que el topic tenga elementos:
+#
+#$KAFKA_HOME/bin/kafka-console-consumer.sh --topic api_data_topic --bootstrap-server localhost:9092 --from-beginning
+
+
+if __name__ == "__main__":
+    kafka_producer()
